7c586963994bb815d3f0ba0edfc2af4300b10c0c
ikins <ikin.musyafa@gmail.com>
2019-05-02 08:04:25 +0700

    button excl

--- resources/views/backend/uo/tni-ad/laporan-rincian-abp.blade.php
+++ resources/views/backend/uo/tni-ad/laporan-rincian-abp.blade.php
@@ -26,7 +26,7 @@
               <div class="box box-solid">
                 <div class="box-body">
                   <div class="row">
-                  <div class="col-md-4">
+                  <div class="col-md-7">
                       <div class="box-body">
                         <table class="table table-border">
                           <tr>
@@ -399,6 +399,13 @@
 <!-- DataTables -->
 <script src="{{ URL('public/datatables.net/js/jquery.dataTables.min.js') }}"></script>
 <script src="{{ URL('public/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js') }}"></script>
 <!-- page script -->
 <script>
   $(function () {
@@ -407,6 +414,10 @@
         {
           "searching": false,
           "bLengthChange": false,
+          dom: 'Bfrtip',
+          buttons: [
+              'excel', 'pdf'
+          ]
         }
 
       );
--- resources/views/backend/uo/tni-au/laporan-rincian-belanja-modal.blade.php
+++ resources/views/backend/uo/tni-au/laporan-rincian-belanja-modal.blade.php
@@ -27,7 +27,7 @@
               <div class="box box-solid">
                 <div class="box-body">
                   <div class="row">
-                  <div class="col-md-4">
+                  <div class="col-md-7">
                       <div class="box-body margin-bottom-20 margin-top-10">
                         <table class="table table-border">
                           <tr>
@@ -176,10 +176,28 @@
 <!-- DataTables -->
 <script src="{{ URL('public/datatables.net/js/jquery.dataTables.min.js') }}"></script>
 <script src="{{ URL('public/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js') }}"></script>
+<script src="{{ URL('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js') }}"></script>
+<script src="{{ URL('https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js') }}"></script>
 <!-- page script -->
 <script>
   $(function () {
-    $('#tablelist').DataTable();
+    $('#tablelist').DataTable(
+
+        {
+          "searching": false,
+          "bLengthChange": false,
+          dom: 'Bfrtip',
+          buttons: [
+              'excel', 'pdf'
+          ]
+        }
+
+      );
   })
 </script>
 @endsection
